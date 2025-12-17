9818a2813a8e10e1c4c8f769071e74d5d4016db8
sulivanShu <regivanx@gmail.com>
2025-12-17 19:07:18 +0100

    Correction nom de variable dans select.jl

--- src/select.jl
+++ src/select.jl
@@ -6,10 +6,7 @@ const GROUP_ID_VEC = @chain begin
 	keys
 	collect
 end
-# TODO: Une constante est plus rapide?
-# const EXACT_SELECTION =
-# TEST: changer en constante pour la production?
-EXACT_SELECTION =
+exact_selection =
 ThreadsX.map(GROUP_ID_VEC) do group_id
 	group_id => select_subgroups(ENTRIES, APPROXIMATE_SELECTION, group_id)
 end |> Dict
