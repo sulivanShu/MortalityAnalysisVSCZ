608879d5ee6b043ed7907d0b52fa9ddae4939ec1
sulivanShu <regivanx@gmail.com>
2025-12-22 22:07:32 +0100

    Supression des individus morts avant leur vaccination avant select.jl

---
+++ src/cleaning.jl
@@ -0,0 +1,10 @@
+@info "Cleaning dataframes"
+ThreadsX.foreach(values(dfs)) do df
+	# garantir l'absence de missing
+	@assert all(!ismissing, df.week_of_dose1)
+	@assert all(!ismissing, df.week_of_death)
+	disallowmissing!(df, [:week_of_dose1, :week_of_death])
+	# supprimer les incohérences temporelles
+	filter!(r -> r.week_of_dose1 <= r.week_of_death, df)
+end
+@info "Cleaning dataframes done"
