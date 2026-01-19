c3286db1cf24cfd8769e7aa586198b4e0d5013df
sulivanShu <regivanx@gmail.com>
2026-01-19 13:47:24 +0100

    Les constantes déclarées dans `Main` n'ont pas besoin d'être déclarées comme globales, car elles le sont déjà

--- src/constants.jl
+++ src/constants.jl
@@ -9,8 +9,8 @@ const UNVACCINATED = Date(10000, 01, 01)
 const STILL_ALIVE = Date(10000, 01, 01)
 const FIRST_MONDAY = Date(2020, 12, 21)
 const LAST_MONDAY = Date(2024, 06, 24)
-global const ALL_MONDAYS = collect(FIRST_MONDAY:Week(1):LAST_MONDAY)
-global const ENTRIES = first(ALL_MONDAYS, length(ALL_MONDAYS)-53)
+const ALL_MONDAYS = collect(FIRST_MONDAY:Week(1):LAST_MONDAY)
+const ENTRIES = first(ALL_MONDAYS, length(ALL_MONDAYS)-53)
 const YEAR_WEEK = ["dose1_week", "death_week"]
 const YEAR_YEAR = ["_5_years_cat_of_birth"]
 
